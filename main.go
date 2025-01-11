ab11ccced004af9046ef9ae4df1cca0feceecd76
mohammad-safakhou <mohammad.sf220@gmail.com>
2025-01-11 14:25:10 +0330

    feat: logs

--- main.go
+++ main.go
@@ -3,26 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"load-test/diameter"
 	"load-test/engine"
 	"load-test/monitoring"
-	"time"
 )
 
 func main() {
-	diameter.CCAs = []string{}
 	numberOfAccounts := flag.Int("num", 1000000, "Number of accounts to create")
+	flag.Parse()
 	fmt.Printf("Number of accounts to create: %d\n", *numberOfAccounts)
 	//monitoring.Init(*numberOfAccounts)
 	engine.Start(*numberOfAccounts)
-	for {
-		time.Sleep(1 * time.Second)
-		if len(diameter.CCAs) == *numberOfAccounts {
-			break
-		} else {
-			fmt.Printf("%d CCA(s) fetched.\n", len(diameter.CCAs))
-		}
-	}
-	fmt.Printf("%d CCA(s) fetched.\n", len(diameter.CCAs))
 	close(monitoring.Monitoring)
 }
